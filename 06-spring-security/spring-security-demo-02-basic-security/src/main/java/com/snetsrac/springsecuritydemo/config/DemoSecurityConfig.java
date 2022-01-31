1f28a1e543d25c56e49eb007bb5f827da7ccf5ba
snetsrac <dev@chriscarstens.com>
2022-01-31 01:13:43 +0000

    Add basic security with default login form

---
+++ 06-spring-security/spring-security-demo-02-basic-security/src/main/java/com/snetsrac/springsecuritydemo/config/DemoSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.snetsrac.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // Add users for in-memory authentication for demo purposes
+        @SuppressWarnings("deprecation")
+        UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("frodo").password("frodo").roles("EMPLOYEE"))
+                .withUser(users.username("aragorn").password("aragorn").roles("MANAGER"))
+                .withUser(users.username("gandalf").password("gandalf").roles("ADMIN"));
+    }
+}
